35867e8130df7e168ab77cbf85cef60b5a92b1f2
Amit-Patra-04 <k2amitpatra@gmail.com>
2025-09-04 18:21:28 +0530

    Scroll Header

--- main.js
+++ main.js
@@ -41,3 +41,10 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
